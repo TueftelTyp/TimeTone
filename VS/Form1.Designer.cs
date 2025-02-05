2d107c05df196f6f7da374f16295f6dcc79a069e
Tüftel Typ <31390221+TueftelTyp@users.noreply.github.com>
2025-02-05 17:14:22 +0100

    vs Code

---
+++ VS/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace TimeTone
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.chkMonday = new System.Windows.Forms.CheckBox();
+            this.chkTuesday = new System.Windows.Forms.CheckBox();
+            this.chkWednesday = new System.Windows.Forms.CheckBox();
+            this.chkThursday = new System.Windows.Forms.CheckBox();
+            this.chkFriday = new System.Windows.Forms.CheckBox();
+            this.chkSaturday = new System.Windows.Forms.CheckBox();
+            this.chkSunday = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
+            this.txtVolVar = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CustomFormat = "HH:mm";
+            this.dtpStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpStart.Location = new System.Drawing.Point(12, 12);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.ShowUpDown = true;
+            this.dtpStart.Size = new System.Drawing.Size(64, 26);
+            this.dtpStart.TabIndex = 0;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.CustomFormat = "HH:mm";
+            this.dtpEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpEnd.Location = new System.Drawing.Point(152, 12);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.ShowUpDown = true;
+            this.dtpEnd.Size = new System.Drawing.Size(64, 26);
+            this.dtpEnd.TabIndex = 1;
+            // 
+            // chkMonday
+            // 
+            this.chkMonday.AutoSize = true;
+            this.chkMonday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMonday.Location = new System.Drawing.Point(81, 50);
+            this.chkMonday.Name = "chkMonday";
+            this.chkMonday.Size = new System.Drawing.Size(75, 20);
+            this.chkMonday.TabIndex = 2;
+            this.chkMonday.Text = "Monday";
+            this.chkMonday.UseVisualStyleBackColor = true;
+            // 
+            // chkTuesday
+            // 
+            this.chkTuesday.AutoSize = true;
+            this.chkTuesday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkTuesday.Location = new System.Drawing.Point(81, 73);
+            this.chkTuesday.Name = "chkTuesday";
+            this.chkTuesday.Size = new System.Drawing.Size(80, 20);
+            this.chkTuesday.TabIndex = 3;
+            this.chkTuesday.Text = "Tuesday";
+            this.chkTuesday.UseVisualStyleBackColor = true;
+            // 
+            // chkWednesday
+            // 
+            this.chkWednesday.AutoSize = true;
+            this.chkWednesday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkWednesday.Location = new System.Drawing.Point(81, 96);
+            this.chkWednesday.Name = "chkWednesday";
+            this.chkWednesday.Size = new System.Drawing.Size(100, 20);
+            this.chkWednesday.TabIndex = 4;
+            this.chkWednesday.Text = "Wednesday";
+            this.chkWednesday.UseVisualStyleBackColor = true;
+            // 
+            // chkThursday
+            // 
+            this.chkThursday.AutoSize = true;
+            this.chkThursday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkThursday.Location = new System.Drawing.Point(81, 119);
+            this.chkThursday.Name = "chkThursday";
+            this.chkThursday.Size = new System.Drawing.Size(83, 20);
+            this.chkThursday.TabIndex = 5;
+            this.chkThursday.Text = "Thursday";
+            this.chkThursday.UseVisualStyleBackColor = true;
+            // 
+            // chkFriday
+            // 
+            this.chkFriday.AutoSize = true;
+            this.chkFriday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFriday.Location = new System.Drawing.Point(81, 142);
+            this.chkFriday.Name = "chkFriday";
+            this.chkFriday.Size = new System.Drawing.Size(64, 20);
+            this.chkFriday.TabIndex = 6;
+            this.chkFriday.Text = "Friday";
+            this.chkFriday.UseVisualStyleBackColor = true;
+            // 
+            // chkSaturday
+            // 
+            this.chkSaturday.AutoSize = true;
+            this.chkSaturday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSaturday.Location = new System.Drawing.Point(81, 165);
+            this.chkSaturday.Name = "chkSaturday";
+            this.chkSaturday.Size = new System.Drawing.Size(80, 20);
+            this.chkSaturday.TabIndex = 7;
+            this.chkSaturday.Text = "Saturday";
+            this.chkSaturday.UseVisualStyleBackColor = true;
+            // 
+            // chkSunday
+            // 
+            this.chkSunday.AutoSize = true;
+            this.chkSunday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSunday.Location = new System.Drawing.Point(81, 188);
+            this.chkSunday.Name = "chkSunday";
+            this.chkSunday.Size = new System.Drawing.Size(72, 20);
+            this.chkSunday.TabIndex = 8;
+            this.chkSunday.Text = "Sunday";
+            this.chkSunday.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TimeTone.Properties.Resources.left;
+            this.pictureBox1.Location = new System.Drawing.Point(95, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Transparent;
+            this.btnStop.BackgroundImage = global::TimeTone.Properties.Resources.stop;
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStop.ForeColor = System.Drawing.Color.Transparent;
+            this.btnStop.Location = new System.Drawing.Point(181, 206);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(36, 35);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "start.png");
+            this.imageList1.Images.SetKeyName(1, "stop.png");
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BackgroundImage = global::TimeTone.Properties.Resources.start;
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStart.ForeColor = System.Drawing.Color.Transparent;
+            this.btnStart.Location = new System.Drawing.Point(12, 206);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(36, 35);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.BackColor = System.Drawing.SystemColors.Control;
+            this.tbVolume.Location = new System.Drawing.Point(54, 214);
+            this.tbVolume.Maximum = 100;
+            this.tbVolume.Minimum = 1;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(121, 45);
+            this.tbVolume.TabIndex = 9;
+            this.tbVolume.Value = 30;
+            this.tbVolume.Scroll += new System.EventHandler(this.tbVolume_Scroll);
+            // 
+            // txtVolVar
+            // 
+            this.txtVolVar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtVolVar.AutoSize = true;
+            this.txtVolVar.BackColor = System.Drawing.Color.Transparent;
+            this.txtVolVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVolVar.Location = new System.Drawing.Point(103, 240);
+            this.txtVolVar.Name = "txtVolVar";
+            this.txtVolVar.Size = new System.Drawing.Size(15, 12);
+            this.txtVolVar.TabIndex = 15;
+            this.txtVolVar.Text = "30";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Consolas", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrl.Location = new System.Drawing.Point(98, 2);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(35, 10);
+            this.lblUrl.TabIndex = 16;
+            this.lblUrl.Text = "GitHub";
+            this.lblUrl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblUrl_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 253);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.txtVolVar);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.chkSunday);
+            this.Controls.Add(this.chkSaturday);
+            this.Controls.Add(this.chkFriday);
+            this.Controls.Add(this.chkThursday);
+            this.Controls.Add(this.chkWednesday);
+            this.Controls.Add(this.chkTuesday);
+            this.Controls.Add(this.chkMonday);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeTone";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.CheckBox chkMonday;
+        private System.Windows.Forms.CheckBox chkTuesday;
+        private System.Windows.Forms.CheckBox chkWednesday;
+        private System.Windows.Forms.CheckBox chkThursday;
+        private System.Windows.Forms.CheckBox chkFriday;
+        private System.Windows.Forms.CheckBox chkSaturday;
+        private System.Windows.Forms.CheckBox chkSunday;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.Label txtVolVar;
+        private System.Windows.Forms.LinkLabel lblUrl;
+    }
+}
+
